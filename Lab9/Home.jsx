45e508f9ec3ae5abd957f40d3d4e24be7fd6c35c
vikki123224v <vighneshwaras283@gmail.com>
2026-04-07 12:14:44 +0530

    Create Home.jsx

---
+++ Lab9/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>🙏Welcome to My Website!🙏</h1>
+      <p>This is the home page of our React Router project.</p>
+
+      <h3>What you can do:</h3>
+      <ul style={{ listStyle: 'none' }}>
+        <li>➡️Visit About page to know more</li>
+        <li>➡️Go to Contact page to reach us</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Home;
