8a5b4e6739028e4c1ef74ee1b688e836cda51fc3
vikki123224v <vighneshwaras283@gmail.com>
2026-04-07 12:15:42 +0530

    Add About component for application description

---
+++ Lab9/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>About Us😀</h1>
+      <p>
+        This is a simple React application demonstrating routing using
+        React Router.
+      </p>
+
+      <p>
+        We use components like Routes, Route, and NavLink to navigate
+        between pages.
+      </p>
+    </div>
+  );
+}
+export default About;
